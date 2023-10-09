224908f337dccab4cceaf3e332b8fa21b0054a3e
todiane <>
2023-10-09 10:30:22 +0000

    Create profile files and add code to models.py, urls, forms and views.py. Update settings with country app (country needs fixing) and added new file to edit profile.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django_countries.fields import CountryField
 
-# Create your models here.
+"""
+Database model for users profile
+"""
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15, default="Usef")
+    last_name = models.CharField(max_length=15, default="Usefsson")
+    date_of_birth = models.DateField(blank=True, null=True)
+    bio = models.TextField(
+        max_length=150, default="Currently no bio", blank=True)
+    photo = CloudinaryField('image', default='placeholder')
+    country = CountryField(
+        max_length=30, blank_label="(Where's your SPOT?)", blank=True
+    )
+
+    def __str__(self):
+        return f'Profile of {self.user} | {self.first_name} {self.last_name}'
--- account/views.py
+++ account/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import LoginForm, UserRegistrationForm, \
+    UserEditForm, ProfileEditForm
+from .models import Profile
 
-
-# ==============================
-# View functions
-# ==============================
 
 def user_login(request):
     if request.method == 'POST':
@@ -32,7 +31,9 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
 
 
 def register(request):
@@ -56,3 +57,29 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated '
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
