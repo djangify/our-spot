bda377907af1327de55ffcc6dafba83905803252
todiane <>
2023-10-10 04:02:10 +0000

    Amend error in main project urls.py file

--- ourspot/urls.py
+++ ourspot/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('images/', include('image.urls', namespace='images')),
+    path('images/', include('images.urls', namespace='images')),
 ]
