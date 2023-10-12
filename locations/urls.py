50e0959ef2bbd02798956f1d17be3e9719243e61
todiane <>
2023-10-12 10:08:03 +0000

    Location details code added to page

--- locations/urls.py
+++ locations/urls.py
@@ -8,8 +8,8 @@ from .views import (
 
 urlpatterns = [
 
-    path("", AddLocation.as_view(), name="add_location"),
-    path("locations/", Locations.as_view(), name="locations"),
+    path("add/", AddLocation.as_view(), name="add_location"),
+    path("", Locations.as_view(), name="locations"),
     path("<slug:pk>/", LocationDetail.as_view(), name="location_detail"),
     path("delete/<slug:pk>/", DeleteLocation.as_view(), name="delete_location"),
     path("edit/<slug:pk>/", EditLocation.as_view(), name="edit_location",)
