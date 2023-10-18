4c8dacb36366a67872918d2a8b57d9402ee1f828
todiane <>
2023-10-18 17:04:53 +0000

    altered photo for profile in model.py

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django_resized import ResizedImageField
 
 
 """
@@ -12,8 +13,13 @@ Database model for users profile
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/',
-                              blank=True)
+    photo = ResizedImageField(
+        size=[300, 300],
+        quality=75,
+        upload_to="account/",
+        force_format="WEBP",
+        blank=True,
+    )
 
     def __str__(self):
         return f'Profile of {self.user.username}'
