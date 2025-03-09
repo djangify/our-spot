299f46fc4b07ddcfeee24a15eb82bab052437d10
Diane Corriette <dcorriette@gmail.com>
2025-03-09 13:06:43 +0000

    Move registration folder back into main project template folder and change all template code to tailwind

--- ourspot/settings.py
+++ ourspot/settings.py
@@ -92,7 +92,7 @@ ROOT_URLCONF = "ourspot.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [TEMPLATES_DIR],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
