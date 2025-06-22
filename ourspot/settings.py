3003e1059c2228965c61d6fd8c105b00c23a5358
djangify <djangify@gmail.com>
2025-06-22 19:47:22 +0100

    Update files

--- ourspot/settings.py
+++ ourspot/settings.py
@@ -2,14 +2,16 @@ from pathlib import Path
 import os
 from django.urls import reverse_lazy
 import environ
-import pymysql 
-
-pymysql.install_as_MySQLdb()
+from decouple import config
 
 # Initialize environment variables
 env = environ.Env()
 
 from dotenv import load_dotenv
+
+# Tell Django to trust the X-Forwarded-Proto header from Apache
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True
 
 
 load_dotenv()
@@ -23,18 +25,29 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'localhost:8000']
+# Update allowed hosts
+ALLOWED_HOSTS = [
+    'showyourspot.com',
+    'www.showyourspot.com',
+    'mail.showyourspot.com',
+    "65.108.89.200",
+    'localhost',
+    '127.0.0.1',
+]
 
+# CSRF_TRUSTED
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://127.0.0.1",
-    "http://localhost:8000",
+    "https://showyourspot.com",
+    "https://www.showyourspot.com",
+    "https://65.108.89.200",
 ]
 
 # SITE ID
 SITE_ID = 1
+
+SITE_URL = 'https://www.showyourspot.com'
 
 # LOGIN URLS
 LOGIN_REDIRECT_URL = "account:dashboard"
@@ -44,21 +57,14 @@ LOGOUT_URL = "account:logout"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": env("DATABASE_NAME"),
-        "USER": env("DATABASE_USER"),
-        "PASSWORD": env("DATABASE_PASSWORD"),
-        "HOST": env("DATABASE_HOST", default="127.0.0.1"),
-        "PORT": env("DATABASE_PORT", default="3306"),
-        "OPTIONS": {
-            "charset": "utf8mb4",
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "use_unicode": True,
-            "connect_timeout": 10,
-            "autocommit": True,
-        },
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default='5432'),
+    }
 }
 
 # Application definition
@@ -68,8 +74,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.sitemaps",
     "django.contrib.sites",
+    "django.contrib.sitemaps",
     "django.contrib.staticfiles",
     "locations",
     "account",
@@ -83,6 +89,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -110,6 +117,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "ourspot.wsgi.application"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -159,6 +169,33 @@ AUTHENTICATION_BACKENDS = [
     "account.authentication.EmailAuthBackend",
 ]
 
+
+# Security settings
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'  # Using local mail server
+EMAIL_PORT = 25  # Standard SMTP port
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+CONTACT_EMAIL = env('CONTACT_EMAIL')
+
+
+# Make sure the logs directory exists
+os.makedirs(os.path.join(BASE_DIR, 'logs'), exist_ok=True)
+
 # Add TinyMCE configuration 
 TINYMCE_DEFAULT_CONFIG = {
     'height': 650,
@@ -188,3 +225,4 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
 }
+
--- ourspot/urls.py
+++ ourspot/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,7 +13,10 @@ urlpatterns = [
     path("locations/", include("locations.urls", namespace="locations")),
     path("blog/", include("blog.urls", namespace="blog")),
     path('tinymce/', include('tinymce.urls')),
+    path('static/<path:path>', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+    path('media/<path:path>', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
